4705e86c00b0bc7cead7d20ab724e69ceca39006
dstroch <d.stroch@gmail.com>
2024-07-04 21:43:38 +0200

    Improve regex to get of meta content defined by attr 'name'

--- goss.sh
+++ goss.sh
@@ -41,8 +41,10 @@ meta_tag_content_by_name() {
 
     # .*>$ - means >$ is end of line and .* is before because content has not to be locate
     # at the end of tag in bad html
-    sed -n -e '1 s!.*<meta.*'"${name}"'.*content=\(.*\).*>$!\1!p; t' -e '1,// s//\1/p' "$filepath" |
-        sed 's!"!!g'
+    # it is a non greedy regex because of [^" >] thats match all char except chars in square brackets
+    # but at the end we have to match set of chars which is not contain in group 1
+    # we except that the content string will end with ", [[:space:]] or >
+    sed -n -e '1 s!.*<meta.*=["]*'"${name}"'["]*.*content=["]*\([^" >]*\)[" >].*$!\1!p; t' -e '1,// s//\1/p' "$filepath"
 }
 
 prepare_help_files() {
